b9f0444b9a13fae25226ab578fc86562be0f6445
vincent viricel <vincent.viricel@gmail.com>
2017-02-21 12:34:58 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  resources :yachts, only: [:index, :show]
+  
+  	namespace :my do
+  		resources :profiles, only: [:show, :edit, :update]
+  		resources :yachts, only: [:new, :create, :edit, :update, :destroy]
+  		resources :bookings, only: [:show, :edit, :update, :new, :create] do
+  			resources :reviews, only: [:create]
+  		end
+  	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
