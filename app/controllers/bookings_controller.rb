a58d1efc40853feaacdfafcdaf7b7bee4939102c
Nelly Duclos <knopfler42@yahoo.fr>
2017-02-22 12:31:45 +0100

    commit before pick up seeds from master

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,24 @@
 class BookingsController < ApplicationController
 
+  before_action :find_yacht, only: [:create]
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def create
-    @booking = Booking.create(params_booking)
+    @booking = Booking.new(booking_params)
+    @booking.yacht = @yacht
+    @booking.save
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :guests, :total_price, :yacht_id, :user_id)
+  end
+
+  def find_yacht
+    @yacht = Yacht.find(params[:yacht_id])
   end
 end
