7feed0ed57faec6bdb859f3d3362a486a34f15c1
Laurene Daccache <laurene.daccache@espeme.com>
2017-02-22 15:23:43 +0100

    geocode without location from search bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,9 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
-//= require_tree .
-
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require underscore
+//= require gmaps/google
+//= require_tree .
