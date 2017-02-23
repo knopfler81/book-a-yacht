26d78cf56f019e6df1820b87b5e51b47bd053836
Nelly Duclos <knopfler42@yahoo.fr>
2017-02-23 17:01:36 +0100

    booking message status

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -2,6 +2,7 @@
 
 
 $( "#confirm" ).click(function() {
-  $("#pay").html("<h4>"  + "You have succsessfully booked" + "</h4>").addClass("booked");
+  // $("#pay").html("<h4>"  + "You have succsessfully booked" + "</h4>").addClass("booked");
+  $('.book-yatch').html("<h4>You have successfully booked</h4>").addClass("booked");
 });
 
