655bfd2531bbf3f8996d82a6557213d5e517c657
vincent viricel <vincent.viricel@gmail.com>
2017-02-24 14:39:25 +0100

    review_hidden

--- app/assets/javascripts/booking.js
+++
@@ -1,8 +0,0 @@
-
-
-
-$( "#confirm" ).click(function() {
-  // $("#pay").html("<h4>"  + "You have succsessfully booked" + "</h4>").addClass("booked");
-  $('.book-yatch').html("<h4>You have successfully booked</h4>").addClass("booked");
-});
-
